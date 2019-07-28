8a07ccf57093fa5dad03dcff117e10f6782f4021
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-07-28 14:05:57 -0400

    Adding user interaction events

--- Ricardo/assets/javascript/script.js
+++ Ricardo/assets/javascript/script.js
@@ -1,7 +1,3 @@
-var searchLocation = 'need to find how to get users location';
-var medicalConditions = [];
-
-
 // BETTER DOCTOR VARIABLES -----------------------------------------------------------------------------------------------------------------------
 const doctors_api_key = '2a424d77579d56c5cb91ae9794c57995';
 var searchField = 'doctors'; // create dropdown list with these options
@@ -15,7 +11,21 @@ const google_maps_api = 'AIzaSyDsCDKZW5hk6Bn6bxiIKnBMUMUB82TAlaM'; // need to ha
 var googleMapsUrl = 'https://maps.googleapis.com/maps/api/staticmap?center=37.773%2c%20-122.413&zoom=12&size=400x400&key=' + google_maps_api
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 
-// FUNCTION gets all symptoms in the BetterDoctor API
+var medicalConditions = [];
+var states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 
+            'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 
+            'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
+            'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
+            'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
+// Create a dropdown list with all the state abbreviations
+for (state of states)
+{
+    $('.states').append('<option value=' + String(state) + '>' + String(state) + '</options>')
+}
+
+
+// FUNCTION gets all conditions in the BetterDoctor API
 $.ajax({
     method: "GET",
     url: doctorsConditions
@@ -30,32 +40,42 @@ $.ajax({
 });
 
 
-function autocomplete(inp, arr)
+function searchLocation(city, state)
 {
-    var currentFocus;
     
-    inp.addEventListener('input', function(e)
-    {
-        console.log(this.value);
-        var a, b, i, val = this.value()
-        
-    })
+    console.log(state.toLowerCase() + '-' + city.toLowerCase())
 }
+
+// EVENT on the dropdown list 
+$('#better-doctor-values').on('change', function(e)
+{
+    $('#input-search').attr('placeholder', 'Search ' + toTitleCase($(this).val()));
+});
 
 
 
 $('#btn-search').on('click', function()
 {
+    
     var dropDownSelection = $('#better-doctor-values').val();
-    betterDoctorsSearch(dropDownSelection);
+    var citySelection = $('#location-search').val();
+    var stateSelection = $('.states').val();
+
+    searchLocation(citySelection, stateSelection);
+    // console.log(stateSelection.toLowerCase() + '-' + citySelection.toLowerCase());
+    // change the placeholder value depending on the dropdown list item selected 
+    
+    // betterDoctorsSearch(dropDownSelection, locationSearch);
+
+    // searchLocation()
 });
 
 
 
-var googleMap = $('<img>');
-googleMap.attr('src', googleMapsUrl);
+// var googleMap = $('<img>');
+// googleMap.attr('src', googleMapsUrl);
 
-$('.map').append(googleMap);
+// $('.map').append(googleMap);
 
 // 1. Use doctors api to be able to show on the html page
 // 2. find out how to extract the 
@@ -64,7 +84,6 @@ $('.map').append(googleMap);
 
 // 37.773,-122.413
 
-<<<<<<< HEAD
 // Run ajax request for doctors before running the one for google maps
 // $.ajax({
 // method: "GET",
@@ -81,68 +100,100 @@ $('.map').append(googleMap);
 // // console.log(response.data[0].practices[0].lon);
 // });
 
-function betterDoctorsSearch(category)
+function betterDoctorsSearch(category, userLocation)
 {
+    var locationSearch = userLocation
+    var searchField;
     if (category === 'doctors')
     {
-        console.log('Doctors');
+        searchField = 'doctors';
+        searchUrl = `https://api.betterdoctor.com/2016-03-01/doctors?location=${locationSearch},100&skip=2&limit=10&user_key=` + doctors_api_key;
     }
     else if (category === 'conditions')
     {
-        console.log('Conditions');
+        searchField = 'conditions';
+        searchUrl = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + doctors_api_key;
     }
     else 
     {
-        console.log('Insurance');
+        searchField = 'insurance'
+        searchUrl = 'https://api.betterdoctor.com/2016-03-01/insurances?skip=0&limit=10&user_key=' + doctors_api_key;
     }
-
-    // var doctorsConditions = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + doctors_api_key;
-    
-    // $.ajax({
-    //     method: "GET",
-    //     url: doctorsConditions
-    // }).then(function(response){
+  
+    $.ajax({
+        method: "GET",
+        url: searchUrl
+    }).then(function(response){
         
-    //     data = response.data
+        console.log(response.data);
+        // data = response.data
         
-    //     for (let i=0; i < data.length; i++)
-    //     {
-    //         console.log(data[i].name);
-    //     }
-    // });
+        // for (let i=0; i < data.length; i++)
+        // {
+        //     console.log(data[i].name);
+        // }
+    });
 }
 
 
-
-// Return a map using the doctors address. Look for pharmacies in the area and plan a route.
-=======
-
-$.ajax({
-method: "GET",
-url: doctorsUrl
-}).then(function(response)
+function toTitleCase(word)
 {
-console.log(response);
-// Capture the latitude through the doctors api
-console.log(response.data[0].practices[0].lat);
-// Capture the longitude the doctors api
-console.log(response.data[0].practices[0].lon);
-});
+    var wordArray = word.toLowerCase().split(' ');
+    for (let i = 0; i < wordArray.length; i++)
+    {
+        wordArray[i] = wordArray[i].charAt(0).toUpperCase() + wordArray[i].slice(1);
+    }
+    return wordArray.join(' ')
+}
 
 
+// function autocomplete(inp, arr)
+// {
+//     var currentFocus;
+    
+//     inp.addEventListener('input', function(e)
+//     {
+        
+//         var a, b, i, val = this.value;
+//         closeAllLists();
+//         if (!val)
+//         {
+//             return false;
+//         }
+//         currentFocus = -1;
+
+//         a = document.createElement('div');
+//         a.setAttribute('id', this.id + 'autocomplete-items');
+//         a.setAttribute('class', 'autocomplete-items');
+
+//         this.parentNode.appendChild(a);
+
+//         for (let i = 0; i < arr.length; i++)
+//         {
+//             if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase())
+//             {
+//                 b = document.createElement('div');
+//                 b.innerHTML = '<strong>' + arr[i].substr(0, val.length) + '</strong>';
+//                 b.innerHTML += arr[i].substr(val.length);
+//                 b.innerHTML += '<input type="hidden" value="' + arr[i] + '">';
+//                     b.addEventListener('click', function(e)
+//                     {
+//                         inp.value = this.getElementsByTagName('input')[0].value;
+//                         closeAllLists();
+//                     });
+//                     a.appendChild(b);
+//             }
+//         }
+//     });
+
+//     inp.addEventListener('keydown', function(e)
+//     {
+//         var x = document.getElementById(this.id + 'autocomplete-list');
+//     })
+// }
+
+// autocomplete(document.getElementById('input-search'), medicalConditions);
 
 
-// User types into an input box with a symtom. The input box should drop down with recommendations of the symptom
-// When the user 
-
-// https://www.doximity.com/developers/home'
-// https://rapidapi.com/palanen/api/betterdoctor-doctor-and-provider-data
-// https://developer.betterdoctor.com/
-// https://developer.betterdoctor.com/code-samples
-// https://cloud.google.com/maps-platform/pricing/
-
-// Health application where the user searches a medical condition. The user gets back information about the 
-// condition and they will also receive a list of doctors in the area with information about the doctors.
 
 // Return a map using the doctors address. Look for pharmacies in the area and plan a route.
->>>>>>> 3229484e5c7f0ad9c3f589e34c3bd5bee31a8f4a
